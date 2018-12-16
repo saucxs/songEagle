58829f26e5ed68e2b24deefd3c6ac3e294362eb1
saucxs <saucxs@163.com>
2018-12-16 12:28:30 +0800

    统计和推送

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,6 +8,33 @@
   </footer>
 </template>
 
+<!--baidu_tongji-->
+<script>
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?7cc624a9cc153ac61e26cb47cb5e3785";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+</script>
+
+<!--baidu_tuisong-->
+<script>
+  (function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+      bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+      bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+  })();
+</script>
+
 <script>
   import axios from 'axios';
   export default {
