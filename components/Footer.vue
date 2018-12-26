b74d39432cd426e47fe323e179ae79dfe01e937e
saucxs <saucxs@163.com>
2018-12-26 23:16:10 +0800

    推送参数

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,6 +7,7 @@
     <span class="after-content">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
     <span class="after-content">本站访客数<span id="busuanzi_value_site_uv"></span>人次</span>
     <!--统计end-->
+    <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
   </footer>
 </template>
 
